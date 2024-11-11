abb3c85f2892b963bd047e5bb034134faa65a64d
kamronnoriyev <kamronnoriyev7@gmail.com>
2024-11-11 22:10:42 +0500

    .

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 
-namespace string3;
+// public class Program
+// {
+//     public static int CountYZ(string text)
+//     {
+//         var word = text.ToLower();
+//         var count = 0;
+//
+//         for (var i = 0; i < word.Length; i++)
+//         {
+//             var letter = word[i];
+//             if ((letter == 'y' || letter == 'z') &&
+//                 (i == word.Length - 1 || !char.IsLetter(word[i + 1])))
+//             {
+//                 count++;
+//             }
+//         }
+//
+//         return count;
+//     }
+//
+//     public static void Main()
+//     {
+//         Console.WriteLine("Satrni kiriting:");
+//         var input = Console.ReadLine();
+//
+//         var result = CountYZ(input);
+//
+//         Console.WriteLine($"'y' yoki 'z' harfi bilan tugaydigan so'zlar soni: {result}");
+//     }
+// }
 
 public class Program
-
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello World!");
+        
     }
 }
